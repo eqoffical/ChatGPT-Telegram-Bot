8c9f549b955cc76156101ce7f5956b1e6a4d107f
RedkoDev <125155438+eqoffical@users.noreply.github.com>
2023-04-11 21:13:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import config
 import logging
 import openai
 import asyncio
+import time
 from aiogram import Bot, Dispatcher, executor, types
 
 # log
 logging.basicConfig(level=logging.INFO)
+
+# launch time
+start_time = time.time()
 
 # init openai
 openai.api_key = config.OPENAI_TOKEN
@@ -14,28 +18,64 @@ openai.api_key = config.OPENAI_TOKEN
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
+# /help command
+@dp.message_handler(commands=['help'])
+async def cmd_help(message: types.Message):
+    await message.answer("Доступні команди:\n"
+                         "/help - Показує всі доступні команди\n"
+                         "/chat - Поговорити з ботом / задати питання\n"
+                         "/status - Показати поточний стан боту")
 
-@dp.message_handler()
+# /start command
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer("Привіт 👋\n"
+                        "Чим я можу вам допомогти?\n"
+                        "Напишіть /help щоб побачити всі доступні команди.")
+
+# /chat command
+@dp.message_handler(commands=['chat'])
+async def cmd_chat(message: types.Message):
+    # remove the '/chat' command from the user's message to get the question
+    question = message.text.replace('/chat', '', 1).strip()
+    if question:
+        model_engine = "text-davinci-003"
+        max_tokens = 1024  # default 1024
+        prompt = question
+        completion = openai.Completion.create(
+            engine=model_engine,
+            prompt=prompt,
+            max_tokens=max_tokens,
+            temperature=0.5,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        await message.answer("Думаю . . .")
+        await message.answer(completion.choices[0].text)
+    else:
+        await message.answer("Будь ласка, напишіть /chat разом зі своїм запитанням.\n"
+                            "Наприклад: /chat коли був створений python?")
+
+
+# /status command
+@dp.message_handler(commands=['status'])
+async def cmd_status(message: types.Message):
+    current_time = time.time() # current time
+    elapsed_time = int(current_time - start_time) # time elapsed since the bot was launched in seconds
+    hours, rem = divmod(elapsed_time, 3600) # converting time to hours, minutes, and seconds
+    minutes, seconds = divmod(rem, 60)
+    
+    status_message = f"Я онлайн і готовий до спілкування!\n"
+    status_message += f"Я тут сижу вже: {hours:02d}:{minutes:02d}:{seconds:02d}"
+    await message.answer(status_message)
+
+# message handler
+@dp.message_handler(commands=['help', 'chat', 'status'])
 async def gpt_answer(message: types.Message):
-    # await message.answer(message.text)
-
-    model_engine = "text-davinci-003"
-    max_tokens = 128  # default 1024
-    prompt = message.text
-    completion = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=max_tokens,
-        temperature=0.5,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-
-    await message.answer("ChatGPT: Генерую відповідь ...")
-    await message.answer(completion.choices[0].text)
+    pass
 
 # run long-polling
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
-
