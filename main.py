b54f129f8a8ba8843e53ec0ba3c04df76138003d
RedkoDev <125155438+eqoffical@users.noreply.github.com>
2023-04-12 17:21:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ import openai
 import asyncio
 import time
 from aiogram import Bot, Dispatcher, executor, types
-from googletrans import Translator
 
 # log
 logging.basicConfig(level=logging.INFO)
 
 # launch time
 start_time = time.time()
-
-# translate
-translator = Translator()
 
 # init openai
 openai.api_key = config.OPENAI_TOKEN
@@ -39,7 +35,7 @@ async def cmd_start(message: types.Message):
                         "Чим я можу вам допомогти?\n"
                         "Напишіть /help щоб побачити всі доступні команди.")
 
-# /chat command
+# chat command
 @dp.message_handler(commands=['chat'])
 async def cmd_chat(message: types.Message):
     # remove the '/chat' command from the user's message to get the question
@@ -58,40 +54,12 @@ async def cmd_chat(message: types.Message):
             presence_penalty=0
         )
 
-        response_text = completion.choices[0].text
-        translation = translator.translate(response_text, dest='uk')
-        await message.answer("Думаю . . .")
-        await message.reply(translation.text)
-    else:
-        await message.reply("Будь ласка, напишіть /chat разом зі своїм запитанням.\n"
-                            "Наприклад: /chat коли був створений python?")
-
-"""
-# chat command (old, without translation)
-@dp.message_handler(commands=['chat'])
-async def cmd_chat(message: types.Message):
-    # remove the '/chat' command from the user's message to get the question
-    question = message.text.replace('/chat', '', 1).strip()
-    if question:
-        model_engine = "text-davinci-003"
-        max_tokens = 1024  # default 1024
-        prompt = question
-        completion = openai.Completion.create(
-            engine=model_engine,
-            prompt=prompt,
-            max_tokens=max_tokens,
-            temperature=0.5,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-
         await message.answer("Думаю . . .")
         await message.reply(completion.choices[0].text)
     else:
         await message.reply("Будь ласка, напишіть /chat разом зі своїм запитанням.\n"
                             "Наприклад: /chat коли був створений python?")
-"""
+
 
 # /status command
 @dp.message_handler(commands=['status'])
@@ -134,4 +102,3 @@ async def gpt_answer(message: types.Message):
 # run long-polling
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
-    
